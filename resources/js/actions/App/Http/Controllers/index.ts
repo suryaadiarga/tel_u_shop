cf5d2eea68e2d87a21789897e232c9fc6f9d0c36
suryaadiarga <suryaadiarga@gmail.com>
2025-09-30 08:31:45 +0700

    First Commit

---
+++ resources/js/actions/App/Http/Controllers/index.ts
@@ -0,0 +1,8 @@
+import Auth from './Auth'
+import Settings from './Settings'
+const Controllers = {
+    Auth: Object.assign(Auth, Auth),
+Settings: Object.assign(Settings, Settings),
+}
+
+export default Controllers
